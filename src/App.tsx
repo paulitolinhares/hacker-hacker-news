ba2cc28d8e1f4f781a2a0d1eace238cc0a72ec7b
Paulo Linhares <paulitolinhares@gmail.com>
2019-08-02 21:10:11 +0200

    testing now deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Hello now</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +19,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
